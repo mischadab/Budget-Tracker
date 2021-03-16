55c1f645220397bb2e2e73d7c64047c92d27bc54
mischadab <mischadabrowski@gmail.com>
2021-03-16 14:52:00 -0400

    service working reeqs

---
+++ public/service-workers.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = "bank-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v2";
+
+const URLS_TO_CACHE = [
+    '/',
+    '/db.js',
+    '/index.js',
+    '/style.css',
+    '/manifest.json',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+// event handler install
