a59d8eb3f7dd294637d551a6d77003bc06e8cf27
mischadab <mischadabrowski@gmail.com>
2021-03-16 16:08:32 -0400

    db on upgrade and on success

--- public/db.js
+++ public/db.js
@@ -6,3 +6,19 @@ const indexedDB =
     window.shimIndexedDB;
 
     let db;
+
+    const request = indexedDB.open("budget", 1);
+
+    request.onupgradeneeded = ({ target }) => {
+        let db = target.result;
+        db.createObjectStore("pending", { autoIncrement: true })
+    };
+
+    request.onsuccess = ({ target }) => {
+        db = target.result;
+
+        // checks if the app is online before reading the database
+        if (navigator.onLine) {
+            checkDatabase();
+        }
+    };
